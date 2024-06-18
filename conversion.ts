72922cb93547e98e8a1ac38d996de57f156d0ebb
nardevvv <vedran.sisic+personal@gmail.com>
2024-06-18 16:27:22 -0400

    Add conversion types

--- conversion.ts
+++ conversion.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export enum ConversionEventType {
   CLICK = "CLICK",
   SWAP = "SWAP",
@@ -51,9 +53,47 @@ type BurnConversionEvent = {
   requirement: BurnConversion;
 };
 
-export type ConversionEvent =
-  | ClickConversionEvent
-  | SwapConversionEvent
-  | VoteConversionEvent
-  | ProgramInteractionConversionEvent
-  | BurnConversionEvent;
+// zod schemas for vonersion
+export const schemaSwapConversion = z.object({
+  inTokenAddress: z.string().optional(),
+  inMinAmount: z.number().optional(),
+  outTokenAddress: z.string().optional(),
+  outMinAmount: z.number().optional(),
+});
+
+export const schemaVoteConversion = z.object({
+  proposal: z.string(),
+});
+
+export const schemaProgramInteractionConversion = z.object({
+  programAddress: z.string(),
+});
+
+export const schemaBurnConversion = z.object({
+  tokenAddress: z.string(),
+  minAmount: z.number().nullish(),
+});
+
+export const schemaConversionEvent = z.discriminatedUnion("eventType", [
+  z.object({
+    eventType: z.literal(ConversionEventType.CLICK),
+  }),
+  z.object({
+    eventType: z.literal(ConversionEventType.SWAP),
+    requirement: schemaSwapConversion,
+  }),
+  z.object({
+    eventType: z.literal(ConversionEventType.VOTE),
+    requirement: schemaVoteConversion,
+  }),
+  z.object({
+    eventType: z.literal(ConversionEventType.PROGRAM_INTERACTION),
+    requirement: schemaProgramInteractionConversion,
+  }),
+  z.object({
+    eventType: z.literal(ConversionEventType.BURN),
+    requirement: schemaBurnConversion,
+  }),
+]);
+
+export type ConversionEvent = z.infer<typeof schemaConversionEvent>;
